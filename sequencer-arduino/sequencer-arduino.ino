709945d0db2fe24a56c3fd3ccc5e520112aebb48
Neil Sveri <neil@dreamsailgames.com>
2018-11-17 17:48:13 -0500

    Sequencer/Midi code

--- sequencer-arduino/sequencer-arduino.ino
+++ sequencer-arduino/sequencer-arduino.ino
@@ -1,18 +1,79 @@
-#include <MIDI.h>
-
-MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 const int channel = 1;
 
-void setup() {
-  MIDI.begin();
+unsigned long tempo = 500;
+
+int lastTick = 0;
+int currentTick = 0;
+int dt = 0;
+
+class SequencerTrack{
+
+public:
+  int step;
+  int steps;
+  int channel;
+  bool isGateOpen;
+  int stepGates [];
+
+  SequencerTrack()
+  {
+
+  }
+
+  SequencerTrack(int trackSteps, int trackChannel)
+  {
+    steps = trackSteps;
+    stepGates [steps] = { };
+    channel = trackChannel;
+  }
+
+  void TickStep()
+  {
+    //Update steps and output.
+    if(isGateOpen)
+    {
+      usbMIDI.sendNoteOff(70 + step%steps, 100, channel);
+      isGateOpen = false;
+    }
+    
+    step++;
+    
+    if(stepGates[step%steps])
+    {
+      usbMIDI.sendNoteOn(70 + step%steps, 100, channel);
+      isGateOpen = true;
+    }
+    
+  }
+  
+};
+
+int trackCount = 1;
+int trackSteps = 8;
+SequencerTrack tracks [1];
+
+void setup(){
+  Serial.begin(9600);
+  
+  //tracks [trackCount] = {};
+  
+  for(int i=0; i<trackCount; i++)
+  {
+    tracks[i] = SequencerTrack(8, i+1); //First track is channel 1;
+  }
 }
 
 void loop() {
-  int note;
-  for (note=10; note <= 127; note++) {
-    MIDI.sendNoteOn(note, 100, channel);
-    delay(200);
-    MIDI.sendNoteOff(note, 100, channel);
+  dt = millis()-lastTick;
+
+  if(millis() - lastTick > tempo)
+  {
+    lastTick = millis();
+    
+    for(int i=0; i<trackCount; i++)
+    {
+      tracks[i].TickStep();
+    }
   }
-  delay(2000);
+  
 }
